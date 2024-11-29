dbfae791228b109238394040494ac11cf08af47c
Razi <razijemni264@gmail.com>
2024-11-29 20:42:38 +0100

    Portfolio

---
+++ .history/scripts_20241127191419.js
@@ -0,0 +1,16 @@
+const pages = document.querySelectorAll(".page");
+const translateAmount = 100;
+let translate = 0;
+
+slide = (index) => {
+    translate = -index * translateAmount;
+    pages.forEach(page => {
+        page.style.transform = `translateX(${translate}%)`;
+    });
+}
+
+document.getElementById("nav-main").addEventListener("click", () => slide(0));
+document.getElementById("nav-about").addEventListener("click", () => slide(1));
+document.getElementById("nav-projects").addEventListener("click", () => slide(2));
+document.getElementById("nav-associations").addEventListener("click", () => slide(3));
+document.getElementById("nav-this").addEventListener("click", () => slide(4));
---
+++ .history/scripts_20241129175031.js
@@ -0,0 +1,41 @@
+const pages = document.querySelectorAll(".page");
+const translateAmount = 100;
+let translate = 0;
+const tabs = document.querySelectorAll(".tab");
+
+slide = (index) => {
+    translate = -index * translateAmount;
+    pages.forEach(page => {
+        page.style.transform = `translateX(${translate}vw)`;
+    });
+    activateTab(index);
+    localStorage.setItem("currentPage", index);
+}
+
+document.getElementById("nav-main").addEventListener("click", () => slide(0));
+document.getElementById("nav-about").addEventListener("click", () => slide(1));
+document.getElementById("nav-projects").addEventListener("click", () => slide(2));
+document.getElementById("nav-this").addEventListener("click", () => slide(3));
+
+activateTab = (index) => {
+    tabs.forEach((tab, idx) => {
+        if (idx === index) {
+            tab.classList.add("active");
+        } else {
+            tab.classList.remove("active");
+        }
+    });
+}
+tabs.forEach((tab, index) => {
+    tab.addEventListener("click", () => {
+        activateTab(index);
+    });
+});
+activateTab(0);
+
+const savedPage = localStorage.getItem("currentPage");
+if (savedPage) {
+    slide(parseInt(savedPage)); // Show the saved page
+} else {
+    slide(0);
+}
